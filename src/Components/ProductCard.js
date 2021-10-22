4175ac80488ef6d9936855ee9e478b3a796fe324
Bilalthoughts <80778230+Bilalthoughts@users.noreply.github.com>
2021-10-22 17:38:21 +0500

    Add files via upload

---
+++ src/Components/ProductCard.js
@@ -0,0 +1,53 @@
+const product_card = [
+    {
+        id:1,
+        product_name: "Heading",
+        description: "Lorem ipsum",
+        price:100,
+        currency: "$",
+        thumb: "./Images/1.jpg",
+    },
+    {
+        id:2,
+        product_name: "Heading",
+        description: "Lorem ipsum",
+        price:100,
+        currency: "$",
+        thumb: "./Images/1.jpg",
+    },
+    {
+        id:3,
+        product_name: "Heading",
+        description: "Lorem ipsum",
+        price:100,
+        currency: "$",
+        thumb: "./Images/1.jpg",
+    },
+    {
+        id:4,
+        product_name: "Heading",
+        description: "Lorem ipsum",
+        price:100,
+        currency: "$",
+        thumb: "./Images/1.jpg",
+    },
+    {
+        id:5,
+        product_name: "Heading",
+        description: "Lorem ipsum",
+        price:100,
+        currency: "$",
+        thumb: "./Images/1.jpg",
+    },
+    {
+        id:6,
+        product_name: "Heading",
+        description: "Lorem ipsum",
+        price:100,
+        currency: "$",
+        thumb: "./Images/1.jpg",
+    },
+    
+]
+
+export default product_card;
